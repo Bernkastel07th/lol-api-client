a23c293d5af0c7e0d2f4891b871ece48ba6e03d4
Bernkastel07th <sato.koki@toiware.co.jp>
2023-01-28 22:36:29 +0900

    update

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@ import HelloWorld from './components/HelloWorld.vue'
     </a>
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+   </a>
   </div>
   <HelloWorld msg="Vite + Vue" />
 </template>
@@ -20,9 +20,11 @@ import HelloWorld from './components/HelloWorld.vue'
   padding: 1.5em;
   will-change: filter;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
